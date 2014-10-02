10e97988ae795132d332d361c97385abc553256d
Guillermo Rauch <rauchg@gmail.com>
2014-10-02 16:51:28 -0700

    fix resulting example filename

--- example/index.js
+++ example/index.js
@@ -7,7 +7,7 @@ var file = grid('sample.mov');
 file.count(64);
 file.interval(1);
 file.render(function(err, buf){
-  var out = join(__dirname, 'grid.jpeg');
+  var out = join(__dirname, 'grid.jpg');
   write(out, buf);
   exec('open ' + out, function(err){
     if (err) return console.log('Grid written to ' + out);
