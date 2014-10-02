2c7e3bbc287a5b6594ab539fcff81e60e04fe25c
Guillermo Rauch <rauchg@gmail.com>
2014-10-02 16:36:33 -0700

    example: refactor example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,15 @@
 var grid = require('..');
+var join = require('path').join;
+var exec = require('child_process').exec;
+var write = require('fs').writeFileSync;
 
-var file = grid('sample.mov', {
-  count: 4,
-  interval: 10
+var file = grid('sample.mov');
+file.count(64);
+file.interval(1);
+file.render(function(err, buf){
+  var out = join(__dirname, 'grid.jpeg');
+  write(out, buf);
+  exec('open ' + out, function(err){
+    if (err) return console.log('Grid written to ' + out);
+  });
 });
