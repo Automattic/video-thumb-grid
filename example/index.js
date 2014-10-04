fca90acaaf547a9c3d57be59e30c8c2f7059a6ae
Guillermo Rauch <rauchg@gmail.com>
2014-10-03 18:16:23 -0700

    refactored example

--- example/index.js
+++ example/index.js
@@ -1,15 +1,16 @@
+var fs = require('fs');
 var grid = require('..');
 var join = require('path').join;
 var exec = require('child_process').exec;
-var write = require('fs').writeFileSync;
 
-var file = grid('sample.mov');
+var input = fs.createReadStream(join(__dirname, 'sample.mp4'));
+var file = grid(input);
 file.count(64);
 file.interval(1);
 file.render(function(err, buf){
   if (err) throw err;
   var out = join(__dirname, 'grid.jpg');
-  write(out, buf);
+  fs.writeFileSync(out, buf);
   exec('open ' + out, function(err){
     if (err) return console.log('Grid written to ' + out);
   });
