4a9b5471f27072a086e6ab9270b6504e6adb6532
Guillermo Rauch <rauchg@gmail.com>
2014-10-04 18:03:16 -0700

    tweak demo

--- example/index.js
+++ example/index.js
@@ -6,6 +6,8 @@ var exec = require('child_process').exec;
 var input = fs.createReadStream(join(__dirname, 'sample.mp4'));
 var file = grid(input);
 file.count(49);
+file.width(240);
+file.height(200);
 file.render(function(err, buf){
   if (err) throw err;
   var out = join(__dirname, 'grid.jpg');
