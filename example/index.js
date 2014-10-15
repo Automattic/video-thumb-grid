c56cd255d62d7b3598c51a24ec1e7701ef5d2422
Guillermo Rauch <rauchg@gmail.com>
2014-10-14 19:51:27 -0700

    index: tweak example to default to previously-problematic width

--- example/index.js
+++ example/index.js
@@ -5,10 +5,13 @@ var exec = require('child_process').exec;
 
 var input = fs.createReadStream(join(__dirname, 'sample.mp4'));
 var file = grid(input);
+var date = new Date;
 file.count(49);
-file.width(240);
-file.height(200);
+file.quality(30);
+file.width(150);
+file.height(100);
 file.render(function(err, buf){
+  console.log('Render completed in %dms', new Date - date);
   if (err) throw err;
   var out = join(__dirname, 'grid.jpg');
   fs.writeFileSync(out, buf);
